dfe75b1d51dce0dc13b83098b178f6b2ff8fedf8
NurSenaAVCI <nursenaavci@hotmail.com>
2024-01-26 16:47:35 +0300

    staticfiles commit

--- ExxenClone/urls.py
+++ ExxenClone/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('home/', include('movie.urls')),
 
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
